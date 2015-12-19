b2ff2531d6dc81f849dd60702d5289cae7f19a04
movetomars <maia.petee@gmail.com>
2015-12-19 10:35:32 -0700

    Finished Bubblesort Method

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,16 +1,22 @@
 def bubblesort(nums)
+    unsorted = true
     
-    (0..nums.size-1).each do |i|
+    (nums.length-1).times do
+        (0..nums.size-1).each do |i|
             
-        if nums[i] > nums[i + 1]
-                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+            if (nums[i] <=> nums[i+1]) == 1
+                nums[i], nums[i+1] = nums[i+1], nums[i]
+                unsorted = false
+            end
+            
         end
-    end
+        
+    end while unsorted
 
     return nums
 end
 
-bubblesort([ 89, 6, 2, 49, 0, 4, 7 ])
+puts bubblesort([ 89, 6, 2, 49, 0, 4, 7 ])
 
 def bubblesort_by()
 
