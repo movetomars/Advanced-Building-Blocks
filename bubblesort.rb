4e0061aeacaec4c12931ba1c87b335305bf3eee6
movetomars <maia.petee@gmail.com>
2015-12-20 13:07:43 -0700

    Finished Bubblesort_by Method

--- bubblesort.rb
+++ bubblesort.rb
@@ -19,9 +19,18 @@ end
 puts bubblesort([ 89, 6, 2, 49, 0, 4, 7 ])
 
 def bubblesort_by(arr)
-    if x <=> y == 1
-        x, y = y, x
-    end
+    unsorted = true
+    
+    (arr.size-1).times do
+        (0..arr.size-1).each do |i|
+            if yield(arr[i] <=> arr[i + 1]) == 1
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                unsorted = false
+            end
+        end
+    end while unsorted
+    
+    arr
 end
 
-puts bubblesort_by(["what", "do", "you", "think", "you're", "doing"]) {|x, y| x.length - y.length}
+puts bubblesort_by(["what", "do", "you", "think", "you're", "doing"]) {|x,y| y.to_s.length - x.to_s.length}
